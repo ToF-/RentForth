a2ac41eec41144fcef56c9c1c9138bd26345bd9e
ToF- <cthibaut@octo.com>
2016-03-15 20:52:28 +0100

    qsort

---
+++ QSort.fs
@@ -0,0 +1,31 @@
+\ QSort
+
+: -CELL -1 CELLS ;
+: CELL- 1 CELLS - ;
+
+: MID ( l,h -- m )
+    OVER - 2 / -CELL AND + ;
+
+: EXCHANGE ( add1,add2 ) 
+    DUP @ -ROT OVER @ SWAP ! ! ;
+
+DEFER  CMP 
+' < IS CMP
+
+: PARTITION ( l,h -- l,h,l',h' )
+    2DUP MID @ >R
+    2DUP BEGIN
+        SWAP BEGIN  DUP @ R@ CMP WHILE CELL+ REPEAT
+        SWAP BEGIN R@ OVER @ CMP WHILE CELL- REPEAT
+        2DUP <= IF 2DUP EXCHANGE >R CELL+ R> CELL- THEN
+    2DUP > UNTIL R> DROP ;
+
+: QSORT ( l,h -- )
+    PARTITION SWAP ROT
+    2DUP < IF RECURSE ELSE 2DROP THEN
+    2DUP < IF RECURSE ELSE 2DROP THEN ;
+
+: SORT ( adr,len -- )
+    DUP 2 < IF 2DROP EXIT THEN
+    1- CELLS OVER + QSORT ;
+
