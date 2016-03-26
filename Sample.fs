0d3a976a62f13c699f7b1261ada310928408303e
ToF- <cthibaut@octo.com>
2016-03-26 10:51:18 +0100

    awk script to prepare orders

---
+++ Sample.fs
@@ -0,0 +1,7 @@
+           INCLUDE Rent.fs INIT-RENT
+        39         10        319 RENT         39         10 + COLLECT
+        53          2        687 RENT         53          2 + COLLECT
+        68          7        183 RENT         68          7 + COLLECT
+        87          2        579 RENT         87          2 + COLLECT
+        99          3        116 RENT         99          3 + COLLECT
+RENT-VALUE ? BYE
