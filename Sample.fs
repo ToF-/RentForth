ecb392f2743c0c45d18f22bba0241383a5b1f6da
ToF- <cthibaut@octo.com>
2016-03-26 20:08:19 +0100

    decode event

--- Sample.fs
+++ Sample.fs
@@ -1,7 +1,7 @@
            INCLUDE Rent.fs INIT-RENT
-        39         10        319 RENT         39         10 + COLLECT
-        53          2        687 RENT         53          2 + COLLECT
-        68          7        183 RENT         68          7 + COLLECT
-        87          2        579 RENT         87          2 + COLLECT
-        99          3        116 RENT         99          3 + COLLECT
-RENT-VALUE ? BYE
+        39         10        319 RENT         39         10 + CASH
+        53          2        687 RENT         53          2 + CASH
+        68          7        183 RENT         68          7 + CASH
+        87          2        579 RENT         87          2 + CASH
+        99          3        116 RENT         99          3 + CASH
+CR RENT-VALUE ? BYE
