2e4373ab0d47dfc63a3e1dc30b74cf8ff609e35b
ToF- <cthibaut@octo.com>
2016-03-28 10:49:22 +0200

    example of execution

--- Sample.fs
+++ Sample.fs
@@ -1,7 +1,10 @@
-           INCLUDE Rent.fs INIT-RENT
-        39         10        319 RENT         39         10 + CASH
-        53          2        687 RENT         53          2 + CASH
-        68          7        183 RENT         68          7 + CASH
-        87          2        579 RENT         87          2 + CASH
-        99          3        116 RENT         99          3 + CASH
-CR RENT-VALUE ? BYE
+INCLUDE Rent.fs
+INIT-RENT
+39  10  319 ADD-ORDER
+53   2  687 ADD-ORDER
+68   7  183 ADD-ORDER
+87   2  579 ADD-ORDER
+99   3  116 ADD-ORDER
+.EVENTS
+COMPUTE-VALUE
+CR . CR BYE
