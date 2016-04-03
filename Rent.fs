f196c82f437a7725efbbbb27d634f467321e7084
ToF- <cthibaut@octo.com>
2016-04-03 16:18:55 +0200

    part 1 and 2 done

--- Rent.fs
+++ Rent.fs
@@ -1,14 +1,22 @@
 \ Rent.fs
 
 REQUIRE ffl/hct.fs
+REQUIRE ffl/act.fs
 
+10000 CONSTANT MAX-ORDERS#
+0 CONSTANT CASH%
+1 CONSTANT RENT%
 VARIABLE HCT-PLAN
 VARIABLE RENT-VALUE
+VARIABLE EVENT-ID
+VARIABLE EVENTS
 
 : INITIALIZE
-    RENT-VALUE OFF
+    RENT-VALUE OFF EVENT-ID OFF
     HCT-PLAN @ ?DUP IF HCT-(FREE) THEN
-    10000 HCT-NEW HCT-PLAN ! ;
+    MAX-ORDERS# HCT-NEW HCT-PLAN ! 
+    EVENTS @ ?DUP IF ACT-(FREE) THEN
+    ACT-NEW EVENTS ! ;
 
 : TIME>STRING ( n -- addr c )
     S>D <# #S #> ;
@@ -27,4 +35,22 @@ VARIABLE RENT-VALUE
 : CASH ( time )
     PLAN@ RENT-VALUE @ MAX RENT-VALUE ! ;
 
+: RUN-EVENT ( time [bid] type -- )
+    RENT% = IF  RENT ELSE CASH THEN ;
+
+: NEXT-ID ( -- id )
+    EVENT-ID DUP @ 1 ROT +! ;
+
+: EVENT-KEY ( time type -- key )
+    SWAP 1 LSHIFT OR 15 LSHIFT NEXT-ID OR ;
+
+: RENT-DATA ( time bid -- data )
+    SWAP 24 LSHIFT OR ;
     
+: ADD-ORDER ( start duration bid -- )
+    >R 2DUP + R> RENT-DATA 
+    2DUP DROP RENT% EVENT-KEY 
+    EVENTS @ ACT-INSERT
+    + CASH% EVENT-KEY 0 SWAP
+    EVENTS @ ACT-INSERT ;     
+
